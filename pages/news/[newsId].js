855fb2f14999aa678ab189116448d0027b4f5fff
Solar5503 <spidervvv@mail.ru>
2022-10-25 16:52:09 +0300

    add nested routes

---
+++ pages/news/[newsId].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+const DetailPage = () => {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+
+  // send a request to the backend API to fetch the news item with newsId
+
+  return <h1>{newsId}</h1>;
+};
+
+export default DetailPage;
